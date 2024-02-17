86827171a5903c9aad2f595d11eafd2db678b665
Arabinda Das <arabinda.mcs@gmail.com>
2024-02-17 20:31:49 +0530

    Update SpringBootCrudExample2Application.java

--- src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
+++ src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.javatechie", "com."}) 
 public class SpringBootCrudExample2Application {
 
 	public static void main(String[] args) {
