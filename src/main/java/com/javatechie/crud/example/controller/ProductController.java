456e99fc60aed42353e91faec0216c53f68aa842
Arabinda Das <arabinda.mcs@gmail.com>
2024-02-17 23:28:12 +0530

    Update ProductController.java

--- src/main/java/com/javatechie/crud/example/controller/ProductController.java
+++ src/main/java/com/javatechie/crud/example/controller/ProductController.java
@@ -8,43 +8,49 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
+    @CrossOrigin
     @PostMapping("/addProduct")
     public Product addProduct(@RequestBody Product product) {
         System.out.println("Saving product");
         return service.saveProduct(product);
     }
 
+    @CrossOrigin
     @PostMapping("/addProducts")
     public List<Product> addProducts(@RequestBody List<Product> products) {
         return service.saveProducts(products);
     }
 
+    @CrossOrigin
     @GetMapping("/products")
     public List<Product> findAllProducts() {
         return service.getProducts();
     }
 
+    @CrossOrigin
     @GetMapping("/productById/{id}")
     public Product findProductById(@PathVariable int id) {
         return service.getProductById(id);
     }
 
+    @CrossOrigin
     @GetMapping("/product/{name}")
     public Product findProductByName(@PathVariable String name) {
         return service.getProductByName(name);
     }
 
+    @CrossOrigin
     @PutMapping("/update")
     public Product updateProduct(@RequestBody Product product) {
         return service.updateProduct(product);
     }
 
+    @CrossOrigin
     @DeleteMapping("/delete/{id}")
     public String deleteProduct(@PathVariable int id) {
         return service.deleteProduct(id);
