97307d13b8dd400c7e8cfceb0c1cc1efa012f54a
Arabinda Das <arabinda.mcs@gmail.com>
2024-02-17 20:31:34 +0530

    Update WebConfig.java

--- src/main/java/com/javatechie/crud/example/WebConfig.java
+++ src/main/java/com/javatechie/crud/example/WebConfig.java
@@ -1,3 +1,5 @@
+package com.javatechie.crud.example;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
