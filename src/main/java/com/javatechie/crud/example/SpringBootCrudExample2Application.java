62bc1b5693bd596fc0050554872ca04096f0c9b9
Arabinda Das <arabinda.mcs@gmail.com>
2024-02-17 23:06:07 +0530

    Update SpringBootCrudExample2Application.java

--- src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
+++ src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootCrudExample2Application {
 
 	public static void main(String[] args) {
+		System.out.println("Starting spring boot application................");
 		SpringApplication.run(SpringBootCrudExample2Application.class, args);
 	}
 
