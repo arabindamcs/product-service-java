4da54946796d4a80fe195daca94bca6ef64d8988
Arabinda Das <arabinda.mcs@gmail.com>
2024-02-21 20:28:46 +0530

    Create User.java

---
+++ src/main/java/com/javatechie/crud/example/entity/User.java
@@ -0,0 +1,33 @@
+// User.java
+package com.javatechie.crud.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USER_TBL")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @ElementCollection(targetClass = String.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role", nullable = false)
+    private Set<String> roles;
+
+    // Constructors, getters, and setters
+}
