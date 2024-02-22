237c72d616ae33141069226f1dd6ff7e6e8a50c6
Arabinda Das <arabinda.mcs@gmail.com>
2024-02-22 19:55:46 +0530

    Update SecurityConfig.java

--- src/main/java/com/javatechie/crud/example/config/SecurityConfig.java
+++ src/main/java/com/javatechie/crud/example/config/SecurityConfig.java
@@ -1,7 +1,6 @@
-package com.javatechie.crud.example.config;
-
 import com.javatechie.crud.example.security.jwt.JwtConfigurer;
 import com.javatechie.crud.example.security.jwt.JwtTokenProvider;
+import com.javatechie.crud.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +8,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -18,6 +19,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    @Autowired
+    private UserService userService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -26,10 +30,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .antMatchers("/api/products").authenticated()
                 .antMatchers(HttpMethod.GET, "/api/products/**").permitAll()
-                .antMatchers("/api/register").permitAll() // Assuming this is your user registration endpoint
+                .antMatchers("/api/register").permitAll()
                 .anyRequest().permitAll()
             .and()
             .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
             .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
